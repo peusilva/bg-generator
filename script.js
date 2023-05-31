12af983e769d2ee9db0f055e9a8c770bbb0b64f2
Pedro Silva <pedro.alsf94@gmail.com>
2023-05-31 17:07:54 -0300

    Prettier

--- script.js
+++ script.js
@@ -2,24 +2,22 @@ let css = document.querySelector("h3");
 let color1 = document.querySelector("#color1");
 let color2 = document.querySelector("#color2");
 let body = document.querySelector("body");
-let button = document.querySelector("#randomColor")
-// let randomColor1 = Math.floor(Math.random()*16777215).toString(16);
-// let randomColor2 = Math.floor(Math.random()*16777215).toString(16);
+let button = document.querySelector("#randomColor");
 color1.value = "#E9DCC9";
 color2.value = "#339999";
 
-
 let setGradient = function () {
-	body.style.background = "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
-	css.innerHTML = body.style.background + ";";
-}
+  body.style.background =
+    "linear-gradient(to right, " + color1.value + ", " + color2.value + ")";
+  css.innerHTML = body.style.background + ";";
+};
 setGradient();
 
 let setRandom = function () {
-	color1.value = "#" + Math.floor(Math.random()*16777215).toString(16);
-	color2.value = "#" + Math.floor(Math.random()*16777215).toString(16);
-	setGradient();
-}
+  color1.value = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  color2.value = "#" + Math.floor(Math.random() * 16777215).toString(16);
+  setGradient();
+};
 
 color1.addEventListener("input", setGradient);
 color2.addEventListener("input", setGradient);
