636c4a568deceddddffdcfafc305467e69e72762
Pedro Silva <pedro.alsf94@gmail.com>
2023-06-05 13:29:03 -0300

    minor

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
+var _= require('lodash');
+
+const arr = [1,2,3,4,5,6,7,8];
+console.log('answer:', _.without(arr, 3));
+
 let css = document.querySelector("h3");
 let color1 = document.querySelector("#color1");
 let color2 = document.querySelector("#color2");
